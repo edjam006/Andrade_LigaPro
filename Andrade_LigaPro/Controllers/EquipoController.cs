c9c0b2a61e85cb789449596c52eceafa5f102001
edjam006 <eduardo.andrade.cedeno@udla.edu.ec>
2025-04-07 09:05:58 -0500

    Respositorio, controlador y vista de Equipos creados.

---
+++ Andrade_LigaPro/Controllers/EquipoController.cs
@@ -0,0 +1,29 @@
+﻿using Andrade_LigaPro.Models;
+using Andrade_LigaPro.Repositorios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andrade_LigaPro.Controllers
+{
+    public class EquipoController : Controller
+    {
+    
+        public ActionResult View()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            EquipoRepository equipoRepository = new EquipoRepository();
+            var equipos = equipoRepository.DevuelveListadoEquipo();
+
+            equipos = equipos.OrderByDescending(item => item.partidosGanados);
+            // equipos = equipos.Where(item => item.Nombre == "Liga de Quito");
+
+
+            return View(equipos);
+        }
+
+
+    }
+}
---
+++ Andrade_LigaPro/Repositorios/EquipoRepository.cs
@@ -0,0 +1,37 @@
+﻿using Andrade_LigaPro.Models;
+
+namespace Andrade_LigaPro.Repositorios
+{
+    public class EquipoRepository
+    {
+        public IEnumerable<equipo> DevuelveListadoEquipo()
+        {
+            List<equipo> equipos = new List<equipo>();
+
+            equipo ldu = new equipo
+            {
+                Id = 1,
+                Nombre = "Liga de Quito",
+                partidosEmpatados = 0,
+                partidosGanados = 10,
+                partidosJugados = 10,
+                partidosPerdidos = 0
+
+            };
+            equipos.Add(ldu);
+            equipo bsc = new equipo
+            {
+                Id = 2,
+                Nombre = "Barcelona Sporting Club",
+                partidosEmpatados = 0,
+                partidosGanados = 9,
+                partidosJugados = 10,
+                partidosPerdidos = 1
+
+            };
+            equipos.Add(bsc);
+            return equipos;
+        }
+
+    }
+}
