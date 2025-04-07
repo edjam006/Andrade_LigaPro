a53e6324af3b92f209a2a8913d00012f15f713a4
laboratorio <laboratorio@UPOAULA10720>
2025-04-07 08:13:58 -0500

    Agregar archivos de proyecto.

---
+++ Andrade_LigaPro/Models/equipo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Andrade_LigaPro.Models
+{
+    public class equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre del equipo")]
+        public String Nombre { get; set; }
+        [Range(0,100)]
+        public int partidosGanados { get; set; }
+        [Range(0, 100)]
+        public int partidosJugados { get; set; }
+        [Range(0, 100)]
+        public int partidosPerdidos { get; set; }
+        [Range(0, 100)]
+        public int partidosEmpatados { get; set; }
+
+
+    }
+}
